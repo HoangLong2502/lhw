77689f87625efb5fa75ffdc639cc02d008f145d8
Hoàng Long <hoanglong2502@Mac-mini-cua-Hoang.local>
2022-09-10 18:08:46 +0700

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,12 +30,24 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <div class="f fdc aic">
+      <Button
+        buttonType='extra-button'
+        buttonIcon="delete"
+        buttonContent='123'/>
+    </div>
   </div>
 </template>
 
 <script>
+import button_system from '@/components/component_system/button_system/button_system.vue';
 export default {
   name: 'HelloWorld',
+  components : {
+    Button : button_system
+  },
+  
   props: {
     msg: String
   }
---
+++ src/components/component_system/button_system/button_system.vue
@@ -0,0 +1,91 @@
+@<template>
+    <div class="button-system">
+        <div 
+            v-if="buttonType === 'main-button'"
+            class="button__item-blue f aic border-radius cursor"
+            :class="buttonPadding">
+            <div 
+                v-if="buttonIcon"
+                class="mr05"
+                :class="'icon--' + buttonIcon">
+            </div>
+            <div
+                :class="buttonFont">
+                {{ this.buttonContent }}
+            </div>
+        </div>
+        <div 
+            v-else-if="buttonType === 'extra-button'"
+            class="button__item-extra f aic border-radius cursor"
+            :class="buttonPadding">
+            <div 
+                v-if="buttonIcon"
+                class="mr05"
+                :class="'icon--' + buttonIcon">
+            </div>
+            <div
+                :class="buttonFont">
+                {{ this.buttonContent }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        buttonIcon : {
+            type : String,
+            default() {
+                return null
+            }
+        },
+
+        buttonContent : {
+            type : String,
+            default() {
+                return ''
+            }
+        },
+
+        buttonType : {
+            type : String,
+            default() {
+                return 'main-button'
+            }
+        },
+
+        buttonStatus : {
+            type : Boolean,
+            default() {
+                return true
+            }
+        },
+
+        buttonSize : {
+            type : String,
+            default() {
+                return 'small'
+            }
+        }
+    },
+
+    mounted () {
+        if (this.buttonSize === 'big') {
+            this.buttonFont = 'h6'
+            this.buttonPadding = 'pt075 pb075 pl1 pr1'
+        }
+    },
+
+    data () {
+        return {
+            buttonFont : 'p5',
+            buttonPadding : 'pt05 pb05 pl075 pr075'
+        }
+    }
+}
+</script>
+
+<style scoped lang='scss'>
+    @import 'button_system';
+</style>
